436a1be79c144f6af6e03efeb4724b074342fc75
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2021-01-16 13:45:09 +0530

    Bringup ssos

--- extract-files.sh
+++ extract-files.sh
@@ -18,7 +18,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 AOSP_ROOT="${MY_DIR}"/../../..
 
-HELPER="${POSP_ROOT}/vendor/potato/build/tools/extract_utils.sh"
+HELPER="${SOSP_ROOT}/vendor/ssos/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -70,11 +70,11 @@ function blob_fixup() {
 }
 
 # Initialize the helper for common device
-setup_vendor "${DEVICE}" "${VENDOR}" "${POSP_ROOT}" true "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${AOSP_ROOT}" true "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
 
-BLOB_ROOT="${POSP_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary"
+BLOB_ROOT="${AOSP_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary"
 
 "${MY_DIR}/setup-makefiles.sh"
